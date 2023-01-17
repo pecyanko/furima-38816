6873af81e8014638025213d158fc1306d2377af5
pecyanko <psar.xx@icloud.com>
2023-01-17 14:28:47 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,11 @@ class User < ApplicationRecord
   validates :nick_name, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birth_day, presence: true
+  validates :password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
+ 
 
   has_many :items
   has_many :orders
