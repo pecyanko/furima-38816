b48f0b44e94f59e51391f798060a018027b3173e
pecyanko <psar.xx@icloud.com>
2023-01-17 19:42:35 +0900

    モデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+    end
+    nick_name             {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation {password}
+    first_name            {"太郎"}
+    first_name_kana       {"タロウ"}
+    last_name             {"山田"}
+    last_name_kana        {"ヤマダ"}
+    birth_day             {Faker::Date.birthday(min_age: 18,max_age: 65)}
+  end
+end
