a5f5e1add501a48a711f9ede37ce0f40ea0cc8c5
pecyanko <psar.xx@icloud.com>
2023-01-18 16:58:11 +0900

    バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
-  
+
   def new
+    ＠item = Item.new
+  end
+
+  def create
+    @item = Item.find(params[:id])
+    @item = Item.new(item_params)
+  end
+
+  private
+  def prototype_params
+    params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :charge_id, :prefecture_id, :delivery_day_id, :cost, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,21 @@ class Item < ApplicationRecord
   belongs_to :Prefecture
   belongs_to :user
 
-  validates :title, :text, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :image, presence: true
+  validates :cost, presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+    validates :cost,numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
+  
 
   validates :prefecture_id, numericality: { other_than: 1 } 
+
 
 end
