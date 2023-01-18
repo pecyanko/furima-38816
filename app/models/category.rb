c133c570dd4299163605b29c11b2e9387aee6c1d
pecyanko <psar.xx@icloud.com>
2023-01-18 16:18:18 +0900

    ActiveHash修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ActiveHash::Base
   self.data = [
-    {id: 1, name: '---'}, {id: 1, name: 'メンズ'}, {id: 1, name: 'レディース'},
-    {id: 1, name: 'ベビー'}, {id: 1, name: 'キッズ'}, {id: 1, name: 'インテリア・住まい・小物'},
-    {id: 1, name: '本・音楽・ゲーム'}, {id: 1, name: 'おもちゃ・ホビー・グッズ'}, {id: 1, name: '家電・スマホ・カメラ'},
-    {id: 1, name: 'スポーツ・レジャー'}, {id: 1, name: 'ハンドメイド'}, {id: 1, name: 'その他'}
+    {id: 1, name: '---'}, {id: 2, name: 'メンズ'}, {id: 3, name: 'レディース'},
+    {id: 4, name: 'ベビー'}, {id: 5, name: 'キッズ'}, {id: 6, name: 'インテリア・住まい・小物'},
+    {id: 7, name: '本・音楽・ゲーム'}, {id: 8, name: 'おもちゃ・ホビー・グッズ'}, {id: 9, name: '家電・スマホ・カメラ'},
+    {id: 10, name: 'スポーツ・レジャー'}, {id: 11, name: 'ハンドメイド'}, {id: 12, name: 'その他'}
     ]
 
     include ActiveHash::Associations
