a4e240998b98f2f568e42a20448abc488a82d05c
pecyanko <psar.xx@icloud.com>
2023-01-19 10:55:52 +0900

    画像の投稿

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,27 @@
 class ItemsController < ApplicationController
-  def index
+
+  before_action :authenticate_user!, except: [:index]
+
+  def index  
   end
 
   def new
-    ＠item = Item.new
+    @item = Item.new
   end
 
   def create
-    @item = Item.find(params[:id])
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-  def prototype_params
-    params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :charge_id, :prefecture_id, :delivery_day_id, :cost, :image).merge(user_id: current_user.id)
-  end
+
+   def item_params
+     params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :charge_id, :prefecture_id, :delivery_day_id, :cost, :image).merge(user_id: current_user.id)
+   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :Prefecture
   belongs_to :user
+  has_one_attached :image
 
   validates :name, presence: true
   validates :introduction, presence: true
