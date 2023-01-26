4b349db9e6fa0def9cb34c9fc3d22f9e9c3ccce3
pecyanko <psar.xx@icloud.com>
2023-01-26 14:44:49 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.order.present? || current_user == @item.user
+      redirect_to root_path 
+    end
   end
 
   def update
